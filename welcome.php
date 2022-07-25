5da4d68e08314aae7f296f728c3ae506ae77786d
Handsome <rikhotsohandsome@gmail.com>
2022-07-25 13:10:26 +0200

    logout and redirection of logged and not logged users, to welcome page

--- welcome.php
+++ welcome.php
@@ -1,8 +1,20 @@
 <?php
 
 session_start();
-$username = $_SESSION['username'];
-$email = $_SESSION['email'];
-echo "welcome ".$username;
+if (isset($_SESSION['username'])) 
+{
+	$username = $_SESSION['username'];
+	$email = $_SESSION['email'];
+?>	
+<link rel="stylesheet" type="text/css" href="style.css">
+<div class="welcome_msg">
+	<h1 align="center">Welcome <?php echo "$username"; ?></h1>
+	<a href="logout.php" class="directLogin">Logout</a>
+</div><?php 
+	
+}
+else{
+	header("Location: index.html");
+}
 
 ?>
